a2643306d5490b54eea17ee298705092dc248cdc
Vinícius <matosvb@gmail.com>
2025-03-03 20:18:06 -0300

    Allow language pack overwriting

--- src/setup/install_lang.sh
+++ src/setup/install_lang.sh
@@ -3,6 +3,6 @@ do
     echo -e "\n[Installing language pack for Moodle: ${lang}]"
     target="/harpia/data/moodledata/lang/${lang}.zip"
     curl -L https://download.moodle.org/download.php/direct/langpack/4.5/${lang}.zip -o "$target"
-    unzip "$target" -d "/harpia/data/moodledata/lang/"
+    unzip -o "$target" -d "/harpia/data/moodledata/lang/"
     rm "$target"
 done
